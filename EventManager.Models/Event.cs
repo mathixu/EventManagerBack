c770d0d33d8797f0a19100d9e626ce44ebbacfd2
mathixu <mathieuroux71@gmail.com>
2024-03-06 13:20:11 +0100

    Add event model - title, description, date, location

---
+++ EventManager.Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace EventManager.Models;
+
+public class Event
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public DateTime Date { get; set; }
+    public string Location { get; set; } = default!;
+}
